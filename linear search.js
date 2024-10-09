560ec2527467790b543b43fc1204f5a636c58cf5
Putu-Bagus-Arjawa <arjawabagus14@gmail.com>
2024-10-09 00:02:08 -0700

    Update searchLinear.js

--- linear search.js
+++ linear search.js
@@ -1,1 +1,12 @@
+let array = [10, 20, 40, 80]
+let target = 10;
 
+function linear(){
+    for (let i = 0; i < array.length; i++){
+        if (target == array[i]){
+            return array[i];
+        }
+    }
+}
+
+console.log(linear());
